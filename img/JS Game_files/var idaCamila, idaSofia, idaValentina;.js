3662799b27fa64efde39bb374ccf6a91300c9fe0
Tucovinegit <20120032@ccpa.com.br>
2025-12-18 14:13:35 -0300

    projeto inicial

---
+++ img/JS Game_files/var idaCamila, idaSofia, idaValentina;.js
@@ -0,0 +1,8 @@
+var idaCamila, idaSofia, idaValentina;
+prompt ("Ingrese la edad de Camila:");
+idaCamila = parseInt(prompt("Insira a idade de Camila:"));
+idaSofia = parseInt(prompt("Insira a idade de Sofia:"));
+idaValentina = parseInt(prompt(" Insira a idade Valentina:"));
+idaCamila < idaSofia 
+idaCamila > idaValentina
+alert (idaCamila);
